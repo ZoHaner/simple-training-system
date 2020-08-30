a11a658bc2493f303622a25d30c0e800f70112e9
Filipp Shkliaev <mod_08@mail.ru>
2020-08-30 20:59:38 +0500

    Clear view

--- Assets/Scripts/Views/DeviceView.cs
+++ Assets/Scripts/Views/DeviceView.cs
@@ -8,21 +8,6 @@ using UnityEngine.EventSystems;
 /// </summary>
 public class DeviceView : MonoBehaviour
 {
-    //private controller
-
-    private Vector2 mousePosition;
-
-    private void Start()
-    {
-        mousePosition = Input.mousePosition;
-    }
-
-    private void LateUpdate()
-    {
-        mousePosition = Input.mousePosition;
-    }
-
-    #region Setters
     public void SetPosition(GameObject gameObject, Vector3 position)
     {
         gameObject.transform.position = position;
@@ -41,23 +26,4 @@ public class DeviceView : MonoBehaviour
             animator.SetBool(paramId, paramValue);
         }
     }
-    #endregion
-
-    #region Senders
-    public void OnDrag(string name)
-    {
-        // controller -> name, , // Input.mousePoisition - mousePosition
-    }
-
-    public void OnEndDrag(string name)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnClick(string name)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    #endregion
 }
