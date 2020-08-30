8e4bbce24105547bdd061623747844981810752a
Filipp Shkliaev <mod_08@mail.ru>
2020-08-30 20:21:38 +0500

    Add DeviceController

---
+++ Assets/Scripts/Controllers/DeviceController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DataModel;
+
+public class DeviceController : MonoBehaviour
+{
+    IDeviceOperations deviceOperations;
+
+    public void OnClick(string name)
+    {
+        deviceOperations.Click(name);
+    }
+
+    public void OnDrag(string name)
+    {
+        deviceOperations.Drag(name, Input.mousePosition);
+    }
+}
